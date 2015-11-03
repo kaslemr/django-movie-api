9c26ecb867930fd9d10c2824c48ea98ebab5dc7d
kaslemr <mattkasle@gmail.com>
2015-11-03 10:52:03 -0500

    bug fix on put function

--- crud/movie/api_views.py
+++ crud/movie/api_views.py
@@ -8,11 +8,11 @@ from movie.models import Movie
 @csrf_exempt
 def api_movie_list_create(request):
     if request.POST:
-        movie = request.POST.get("title")
-        Movie.objects.create(movie=movie)
+        title = request.POST.get("title")
+        Movie.objects.create(title=title)
         return HttpResponse(json.dumps({"message": "Success"}), content_type="application/json")
     all_movies = Movie.objects.all()
-    data = [{"id": movie.id, "movie": movie.title} for movie in all_movies]
+    data = [{"id": movie.id, "title": movie.title} for movie in all_movies]
     data = json.dumps(data)
     return HttpResponse(data, content_type="application/json")
 
@@ -20,3 +20,4 @@ def api_movie_detail(request, pk):
     movie = Movie.objects.get(id=pk)
     serialized_movie = json.dumps({"id": movie.id, "movie": movie.title})
     return HttpResponse(serialized_movie, content_type="application/json")
+
